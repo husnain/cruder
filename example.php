63c3d679615ac63c65118db173d12c54b57d4dab
Husnain Ahmed <husnain.ahmed@skillorbit.com>
2021-08-12 15:25:11 +0500

    License added

---
+++ example.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'vendor/autoload.php';
+ 
+use Cruder\Sharp\SharpCrud as Cruder;
+ $insert_department = array('dept_no' => 'd010', 'dept_name' => 'Test' );
+$cruder = Cruder::getInstance('localhost', 'root', '', 'employees');
+
+
+$cruder->table('employees');
+$result = $cruder->getAll();
+
